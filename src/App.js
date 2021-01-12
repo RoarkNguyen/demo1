88ff56bdbb7b1cb337b6a089b97e1d53671e0382
Hoang Nguyen <hoangnguyen220396@gmail.com>
2021-01-12 09:12:59 +0700

    change app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src="https://media.doisongphapluat.com/684/2020/7/21/gai-xinh-tha-thinh-bang-nu-hon-ngot-ngao-nhieu-nam-sinh-hi-hung-cho-doi-va-cai-ket-cuoi-ra-nuoc-mat-dspl-1.jpg" className="App-logo" width="300" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and.aaaaaaaaaaaaaaa
+          Edit <code>src/App.js</code> and.aaaaaaaaaaaaaaasdfdsfsd
         </p>
         <a
           className="App-link"
